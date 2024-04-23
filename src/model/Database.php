b0e781366e70009c060726a33c96e22449819bd6
EcharpeRouge <zawiczrczarny@gmail.com>
2024-04-23 11:58:33 +0200

    Ajout de la classe Database et entamer le CRUD

---
+++ src/model/Database.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\CTProject\model;
+
+class Database
+{
+    private static ?Database $_instance=null;
+    private ?\PDO $_connexion = null;
+
+    private function __construct()
+    {
+        $this->_connexion = new \PDO('mysql:host=mysql-server;dbname=CTDB', 'db_user', 'example');
+    }
+
+    public static function getInstance()
+    {
+        if (self::$_instance ==null)
+            self::$_instance = new Database();
+      
+            return self::$_instance;
+    }
+    public function getConnexion():\PDO
+    {
+        return $this->_connexion;
+    }
+
+}
--- src/model/Messages.php
+++ src/model/Messages.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 namespace App\CTProject\model;
-
 use DateTime;
 
 class Messages
@@ -43,5 +42,35 @@ class Messages
     public function setMessageDate(DateTime $currentDate):void
     {
         $this->_messageDate = $currentDate;
+    }
+
+    public static function createMessage (Messages $messages)
+    {
+        $statement=Database::getInstance()->getConnexion()->prepare("INSERT INTO `MESSAGES` (id,messageSent,messageDate,userId) values (:id,:messageSent,:messageDate,:userId);");
+        $statement->execute(['id'=>$messages->getId(),'messageSent'=>$messages->getMessageSent(),'messageDate'=>$messages->getMessageDate(),'userId'=>$messages->getUserId()]);
+        return Database::getInstance()->getConnexion()->lastInsertId();
+    }
+
+    public static function updateMessage(Messages $message)
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('UPDATE `MESSAGES` set messageSent=:messageSent WHERE id =:id');
+        $statement->execute(['messageSent'=>$message->getMessageSent(), 'id'=>$message->getId()]);
+    }
+
+    public static function readMessage(int $id):Messages
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('SELECT * FROM `MESSAGES` WHERE id=:id');
+        $statement->execute(['id' => $id]);
+        if ($row = $statement->fetch())
+        {
+            $message = new Messages(id:$row['id'], messageSent:$row['messageSent'], messageDate:$row['messageDate'], userId:$row['userId']);
+            return $message;
+        }
+        return null;
+    }
+    public static function deleteMessage(Messages $id)
+    {
+        $statement = Database::getInstance()->getConnexion()->prepare('DELETE FROM `MESSAGES` where id=:id');
+        $statement->execute(['id'=>$id->getId()]);
     }
 }
