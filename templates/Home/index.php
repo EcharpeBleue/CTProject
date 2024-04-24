7ecc3361ea5f1505853b5cda49ff9d69032b500e
EcharpeRouge <zawiczrczarny@gmail.com>
2024-04-24 17:17:08 +0200

    organisation des fichiers scripts/css dans le layout

--- templates/Home/index.php
+++ templates/Home/index.php
@@ -1,1 +1,71 @@
-Bienvenue sur le site!
+<html>
+<body>
+    <section id="headerArea">
+        Cosne Transports
+        <div id="linksContainer">
+            <a class="linksArea" href=""> Machin </a>
+            <a class="linksArea" href=""> Truc </a>
+            <a class="linksArea" href=""> Bidule </a>
+        </div>
+    </section>
+    <div id="carouselContainer">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="3000">
+                    <img src="./assets/imgs/img1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="3000">
+                    <img src="./assets/imgs/img2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="3000">
+                    <img src="./assets/imgs/img3.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <button id="pauseButton" class="btn btn-warning">Pause</button>
+    <button id="playButton" class="btn btn-success">Play</button>
+
+    <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated <?php echo date('Y-m-d H:i:s'); ?></small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated <?php echo date('Y-m-d H:i:s'); ?></small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated <?php echo date('Y-m-d H:i:s'); ?></small>
+    </div>
+  </div>
+</div>
+</body>
+
+</html>
